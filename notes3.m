a3d13426c678a963f492085f3622d6e594dd1f04
Minhyuk Nam <minhyuk.nam@outlook.com>
2018-03-19 17:41:55 +0900

    Update and rename note3.m to notes3.m

--- notes3.m
+++ notes3.m
@@ -5,7 +5,7 @@
 %% CHP 3-1. Flow Control
 %% Repetition(1): for Loop
 a = 0;
-for i = 1:10 %초기값 0으로 함으로써, i를 1부터 시작함
+for i = 1:10 %초기값 0으로 함으로써, i를 1부터 시작함, i goes from 1 to 10
     a = a + i;
 end
 disp(a)
@@ -33,7 +33,7 @@ end
    
 %% Conditional Statement(2-1): Switch
 a = 0; b = 0;
-for i = 1:20
+for i = 1 : 20
     switch i
         case 1
             disp('HI')
@@ -53,7 +53,7 @@ for i = 1:11
         case {2, 4, 6, 8, 10}
             b = b + i;
         otherwise
-            error('Quit') %show error message
+            error('Quit') %show error message; exploit this function to check each section
 	end
 end
 
@@ -74,5 +74,44 @@ end
 
 disp([a, b, c])
 
+%% Input
+a = 0; b = 0;
+n = input('Enter number between 1 and 10: ')
+for i = 1 : n
+    switch i
+        case {1, 3, 5, 7, 9}
+            a = a + i;
+        case {2, 4, 6, 8, 10}
+            b = b + i;
+        otherwise
+            warning('warniing') %warning() function does not quit the programming
+    end
+end
+
+disp([a, b])
+
+%% Menu
+a = 0; b = 0;
+n = menu('select', 'Red', 'Green');
+
+if n == 1
+    n = 10;
+else
+    n = 20;
+end
+
+for i = 1 : n
+    switch i
+        case {1, 3, 5, 7, 9}
+            a = a + i;
+        case {2, 4, 6, 8, 10}
+            b = b + i;
+        otherwise
+            warning('warning')
+    end
+end
+
+disp([a, b])
+
 %% CHP 3-2. Image Visualisation
-%%
+%% 
