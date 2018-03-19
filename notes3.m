5b2f01828d446239e110081f512b24f212af485b
Minhyuk Nam <minhyuk.nam@outlook.com>
2018-03-19 18:10:09 +0900

    Update notes3.m

--- notes3.m
+++ notes3.m
@@ -2,6 +2,11 @@
 % DATE: 19/03/2018
 % AUTHOR: MINHYUK NAM
 
+%% Contents
+% * Flow Control
+% * Operations
+% * Function Handling
+
 %% CHP 3-1. Flow Control
 %% Repetition(1): for Loop
 a = 0;
@@ -92,7 +97,7 @@ disp([a, b])
 
 %% Menu
 a = 0; b = 0;
-n = menu('select', 'Red', 'Green');
+n = menu('Choose a color', 'Red', 'Green'); %this function assigens number to n as 1 or 2
 
 if n == 1
     n = 10;
@@ -113,5 +118,49 @@ end
 
 disp([a, b])
 
-%% CHP 3-2. Image Visualisation
-%% 
+%% CHP 3-2. Operators
+1+1
+2*39
+1/9
+i*i
+10*10
+10^2
+234456356314 %floating point is default
+format long %shows longer digits
+234456356314 
+format short %shows shorter digits
+exp(1) %expon. to 1
+exp(2) %expon. to 2
+log(2.7183) %natural log
+log10(10) %
+sqrt(4) %square root
+cos(pi) %cosine(pi)
+cos(3.14) %difference between pi and 3.14
+x = 1 %NOT 1 = x for assignment
+x + 2
+
+%% CHP 3-3. Function Handle
+%% User Defined Function
+F = @(A,x) A*cos(x) %note that A and x are both parameter for function F
+F(2, pi) %equiv to a*cos(pi)
+S = @(x) sin(x)
+G = @(x) 17*S(x) %recursively defined
+G(pi)
+H = @cos
+I = @exp
+
+%% Polynomial Function
+f = @(x) x^2+4*x
+f(2)
+% f(1:5) is not possible
+
+i = 0;
+for i = 1:5
+    f(i)
+end
+
+g = @(x) x.^2+4*x %use .^ for above degree 2
+g(1:5) %let function to compute with each elements in the given array
+
+gdiff = @(x) g(x)-g(x-1) %possible to compose function
+gdiff(2)
