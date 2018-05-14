da8a445b1232eb01352a3630c2147291ac27bca2
Minhyuk Nam <minhyuk.nam@outlook.com>
2018-05-14 17:47:21 +0900

    Update notes9.m

--- notes9.m
+++ notes9.m
@@ -3,7 +3,7 @@
 % DATE: 05/14/2018
 % AUTHOR: MINHYUK NAM
 
-%% 1. Limitation
+%% 1. Limitation, Differentiation and Integral
 % syms ; do not use comma(,) to differentiate each variables
 % limit(<things to limit), <smth>, <goes to somewhere>)
 % diff(<function>, <with respect to>)
@@ -27,6 +27,7 @@ int(1/x, x, 1, 20) % definite integral
 double(ans) % equiv. to double(int(1/x, x, 1, 20))
 
 int(1/x, x) % indef. integral
+
 %% 1-2. Further Usage
 f(x) = cos(x), g(x) = sin(x)
 diff(f(x)*g(x), x) % differentiation of multiplication
@@ -35,6 +36,48 @@ simplify(ans) % to make simplier result
 diff(f(x)/g(x), x) % differentiation of fraction
 simplify(ans)
 
-%% 2. 
+%% 1-3. Others
+int(x^x, x) % since indef. integral of x^x w.r.t. x is unknown, 
+double(int(x^x, x, 1, 2)) % approximation can be calculated
 
-%% 3. 
+%% Assignment
+syms x
+f(x) = x^2 + x + 3
+g(x) = cos(x)
+h(x) = exp(x)
+
+double(limit(f(x), x, 10))
+double(limit(g(x), x, 10))
+double(limit(h(x), x, 10))
+
+subs(diff(f(x), x), 10)
+subs(diff(g(x), x), 10)
+subs(diff(h(x), x), 10)
+
+int(f(x), x, 1, 10)
+int(g(x), x, 1, 10)
+int(h(x), x, 1, 10)
+
+%% 2. ODE45 SOLVER
+% [t, solutions] = ode(<problems name to call>, <t values>, <initial value of sols>)
+% REQUIRE M FILES TO USE THIS SOLVER
+
+% BELOW FUNCTION IS DEFINED AS ```EPIDEMIC``` IS CREATED
+% function SEQ = epidemic(t, sols)
+% SEQ = zeros(size(sols)); % matrix size is known after it is calculated
+% 
+%```BELOW PART IS SET OF CONSTANTS```
+% a = 0.001;
+% b = 0.2;
+% 
+% S = sols(1);
+% I = sols(2);
+% R = sols(3);
+% 
+% ```BELOW PART IS SET OF ALGEBRA (system of ODE)```
+% SEQ(1) = (-a) * S * I;
+% SEQ(2) = a * S * I - b * I;
+% SEQ(3) = b * I;
+
+[t, solutions] = ode45('epidemic', 1:0.5:50, [2180, 1, 0]);
+plot([t, solutions]), legend
